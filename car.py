bca8752dc8f49311ea20c430f33ac78ee59ab4a5
acevedog <sturdyprogrammer@gmail.com>
2025-10-06 20:07:23 -0400

    Change variable from self.gallonsOfGas to self.currentGallonsOfGas

--- car.py
+++ car.py
@@ -1,8 +1,9 @@
 class Car:
     def __init__(self, gallonsOfGas):
         print ("car created")
-        self.gallonsOfGas = gallonsOfGas
+        self.currentGallonsOfGas = gallonsOfGas
         self.maxGallonOfGas = 12
+        # number of miles per gallon. Then howMuchToDrive (amountOfMIles), then costs of gallons of gasoline, then newAmountOfGasoline = self.currentAmountOfGas - numOfGallons it took to  move
 
     def turnOn(self):
         print ("Battery OK")
@@ -14,7 +15,7 @@ class Car:
         print ("Engine OK")
 
     def fillWithGas(self, numToFillBy):
-        newAmountOfGas = self.gallonsOfGas + numToFillBy
+        newAmountOfGas = self.currentGallonsOfGas + numToFillBy
         if newAmountOfGas > self.maxGallonOfGas:
             raise Exception("The car can't hold that much of gas because you put in {} gallons, and the max is {} gallons, and it's {} gallons over!".format(newAmountOfGas, self.maxGallonOfGas, newAmountOfGas - self.maxGallonOfGas))
 
