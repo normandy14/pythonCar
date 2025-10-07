f25426e5136f77d6b55851dfe3394eb219ba769b
acevedog <sturdyprogrammer@gmail.com>
2025-10-06 20:45:43 -0400

    Create fillWithGas pytest

--- test_app.py
+++ test_app.py
@@ -1,4 +1,15 @@
 from car import Car
+import pytest
 
 def test_helloWorld():
     assert "helloworld" == "helloworld"
+
+def test_fillWithGas_error():
+    """
+    Test that the exception has too much gas
+    """
+    with pytest.raises(Exception) as excinfo:
+        car = Car(3)
+        car.fillWithGas(15)
+        print (excinfo)
+    assert str(excinfo.value) == "The car can't hold that much of gas because you put in 18 gallons, and the max is 12 gallons, and it's 6 gallons over!"
